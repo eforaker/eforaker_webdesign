4607e740735b34e63e05afa677f00f5673f5747e
eforaker <35076496+eforaker@users.noreply.github.com>
2018-01-20 00:01:42 -0500

    12 am

--- Petey24/script.js
+++ Petey24/script.js
@@ -3,4 +3,25 @@ $(document).ready(function() {
 	$('.cat').draggable();
 	$('.ball').draggable();
 	$('.ball2').draggable();
-  });
+$('.breakfast').each(function() {
+    var elem = $(this);
+    setInterval(function() {
+        if (elem.css('visibility') == 'hidden') {
+            elem.css('visibility', 'visible');
+        } else {
+            elem.css('visibility', 'hidden');
+        }    
+    }, 750);
+});
+$('.dinner').each(function() {
+    var elem = $(this);
+    setInterval(function() {
+        if (elem.css('visibility') == 'hidden') {
+            elem.css('visibility', 'visible');
+        } else {
+            elem.css('visibility', 'hidden');
+        }    
+    }, 750);
+});
+});
+
