b183a95bd57787f13c05f78ec83f46703e3fe715
eforaker <35076496+eforaker@users.noreply.github.com>
2018-01-26 09:00:59 -0500

    9am

---
+++ Final Portfolio/projects.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+	$('.container1').draggable();
+	$('.container2').draggable();
+	$('.container3').draggable();
+	$('.container4').draggable();
+	$('.container5').draggable();
+});
