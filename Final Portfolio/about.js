7b7dbb1ff5f20abe9bb849b4c4f7a9dc0cb340fc
eforaker <35076496+eforaker@users.noreply.github.com>
2018-01-27 13:14:31 -0500

    1:15

--- Final Portfolio/about.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-	$('.container1').draggable();
-	$('.container2').draggable();
-	$('.container3').draggable();
-	$('.container4').draggable();
-	$('.container5').draggable();
-});
---
+++ Final Portfolio/sketches/sketches.js
@@ -0,0 +1,14 @@
+$(function() {
+//----- OPEN
+$('[data-popup-open]').on('click', function(e)  {
+var targeted_popup_class = jQuery(this).attr('data-popup-open');
+$('[data-popup="' + targeted_popup_class + '"]').fadeIn(10);
+e.preventDefault();
+});
+//----- CLOSE
+$('[data-popup-close]').on('click', function(e)  {
+var targeted_popup_class = jQuery(this).attr('data-popup-close');
+$('[data-popup="' + targeted_popup_class + '"]').fadeOut(10);
+e.preventDefault();
+});
+});
