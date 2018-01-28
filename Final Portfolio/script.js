147d293f526fadc6b6593b35b36650624fc851f8
eforaker <35076496+eforaker@users.noreply.github.com>
2018-01-28 12:33:49 -0500

    12:30

--- Final Portfolio/script.js
+++ Final Portfolio/script.js
@@ -4,4 +4,28 @@ $(document).ready(function() {
 	$('.container3').draggable();
 	$('.container4').draggable();
 	$('.container5').draggable();
+	$('.container6').draggable();
+	$('.container7').draggable();
+
+	$('.container1').click(function() {
+    $(this).fadeOut('fast');
+    });
+    $('.container2').click(function() {
+    $(this).fadeOut('fast');
+	});
+    $('.container3').click(function() {
+    $(this).fadeOut('fast');
+	});
+	$('.container4').click(function() {
+    $(this).fadeOut('fast');
+    });
+    $('.container5').click(function() {
+    $(this).fadeOut('fast');
+	});
+    $('.container6').click(function() {
+    $(this).fadeOut('fast');
+	});
+	$('.container7').click(function() {
+    $(this).fadeOut('fast');
+	});
 });
