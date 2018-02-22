58a0333423abb04213c5668839d1585f4724e008
eforaker <35076496+eforaker@users.noreply.github.com>
2018-02-21 21:05:36 -0500

    9:05pm

--- anitdepressants/script.js
+++ anitdepressants/script.js
@@ -1,3 +1,33 @@
 $(document).ready(function() {
+    $('#PY1 img').draggable();
+    $('#YB1 img').draggable();
+    $('#RY1 img').draggable();
     $('.pill65 img').draggable();
+    $('.pill2x img').draggable();
+    $('.pillquarter img').draggable();
+
+    $('#RY1 img').click(function() {
+    $(this).fadeOut('slow');
+    });
+    $('#YB1 img').click(function() {
+    $(this).fadeOut('slow');
+	});
+    $('#PY1 img').click(function() {
+    $(this).fadeOut('slow');
+	});
+	$('#threepills img').click(function() {
+    $(this).fadeOut('slow');
+    });
+    $('.pillquarter img').click(function() {
+    $(this).fadeOut('slow');
+	});
+    $('.pill2x img').click(function() {
+    $(this).fadeOut('slow');
+	});
+	$('.pill65 img').click(function() {
+    $(this).fadeOut('slow');
+	});
+	$('#GY1 img').click(function() {
+    $(this).fadeOut('slow');
+    });
 });
