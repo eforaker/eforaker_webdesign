ac0f8c991f3e52a91f356a0b21068ae078704509
eforaker <35076496+eforaker@users.noreply.github.com>
2018-11-14 18:04:31 -0500

    6pm

--- Judgement Day/judge.js
+++ Judgement Day/judge.js
@@ -1,8 +1,3 @@
 $(document).ready(function() {
     $('.images img').draggable();
-
-   //  $(function() {
-   //  var images = ['roses.gif', 'sky.gif'];
-   //  $('background').css({'background-image': 'url(images/' + images[Math.floor(Math.random() * images.length)] + ')'});
-   // });
 });
