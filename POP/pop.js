45c066203a147933a448ebb656a38331bbf3dabe
eforaker <35076496+eforaker@users.noreply.github.com>
2018-11-28 00:44:03 -0500

    1244

--- POP/pop.js
+++ POP/pop.js
@@ -1,3 +1,10 @@
 $(document).ready(function() {
     $('.images img').draggable();
+
+  var audio = document.getElementsByTagName("audio")[0];
+  $("#britney2").click(function() {
+  	  console.log("play");
+      if (audio.paused) audio.play();
+    else audio.pause();
+  });
 });
