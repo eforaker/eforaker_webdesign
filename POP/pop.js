4a83cf78061f7a3ba356fb4cadb00dc78108bef8
eforaker <35076496+eforaker@users.noreply.github.com>
2018-11-28 16:23:53 -0500

    4

--- POP/pop.js
+++ POP/pop.js
@@ -1,8 +1,29 @@
 $(document).ready(function() {
     $('.images img').draggable();
 
-  var audio = document.getElementsByTagName("audio")[0];
+  var audio = document.getElementsByTagName('#crazy audio')[0];
   $("#britney2").click(function() {
+  	  console.log("play");
+      if (audio.paused) audio.play();
+    else audio.pause();
+  });
+
+    var audio = document.getElementsByTagName('#hitme audio')[0];
+  $("#britney3").click(function() {
+  	  console.log("play");
+      if (audio.paused) audio.play();
+    else audio.pause();
+  });
+
+    var audio = document.getElementsByTagName('audio')[0];
+  $("#britney4").click(function() {
+  	  console.log("play");
+      if (audio.paused) audio.play();
+    else audio.pause();
+  });
+
+    var audio = document.getElementsByTagName('audio')[0];
+  $("#britney5").click(function() {
   	  console.log("play");
       if (audio.paused) audio.play();
     else audio.pause();
