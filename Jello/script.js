9367a48288ce661796fcf67c830c630f3f023623
eforaker <35076496+eforaker@users.noreply.github.com>
2019-02-22 17:56:34 -0500

    6pm

--- Jello/script.js
+++ Jello/script.js
@@ -1,4 +1,8 @@
 $(document).ready(function() {
-    $('#jello').draggable();
 
- });
+
+var audio = document.getElementsByTagName("audio")[0];
+  $(".jello").mouseenter(function() {
+  audio.play();
+  });
+   });
