79ac6ba78652ed98ee30442d12ebcbbff4f732e2
eforaker <35076496+eforaker@users.noreply.github.com>
2019-09-17 17:56:28 -0400

    556

---
+++ exceltypespec/specimen.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $('.images img').draggable();
+
+   //  $(function() {
+   //  var images = ['roses.gif', 'sky.gif'];
+   //  $('background').css({'background-image': 'url(images/' + images[Math.floor(Math.random() * images.length)] + ')'});
+   // });
+});
