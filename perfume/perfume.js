1464d12162001f1951d2740cf9ab0037902a920a
eforaker <35076496+eforaker@users.noreply.github.com>
2020-01-27 10:38:47 -0500

    1038am

--- perfume/perfume.js
+++ perfume/perfume.js
@@ -1,6 +1,3 @@
-  var audio = document.getElementsByTagName('audio')[0];
-  $(".bottle img").click(function() {
-  	  console.log("play");
-      if (audio.paused) audio.play();
-    else audio.pause();
-  });
+$(document).ready(function() {
+    $('.bottle img').draggable();
+    });
