b28d0403ec8a701e0a499882e1909468cfe21968
eforaker <35076496+eforaker@users.noreply.github.com>
2020-02-01 18:29:45 -0500

    629

--- perfume/perfume.js
+++ perfume/perfume.js
@@ -1,3 +1,16 @@
-$(document).ready(function() {
-    $('.bottle img').draggable();
-    });
+var myAudio = new Audio('briteverytime.mp3');
+var isPlaying = false;
+
+function playAudio() {
+  if (isPlaying) {
+    myAudio.pause()
+  } else {
+    myAudio.play();
+  }
+};
+myAudio.onplaying = function() {
+  isPlaying = true;
+};
+myAudio.onpause = function() {
+  isPlaying = false;
+};
