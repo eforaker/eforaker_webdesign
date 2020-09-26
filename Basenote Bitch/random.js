24353b43518798f57b9683409612c0e6ed1fdbbb
eforaker <35076496+eforaker@users.noreply.github.com>
2020-09-26 19:04:35 -0400

    704

---
+++ Basenote Bitch/random.js
@@ -0,0 +1,46 @@
+
+var random_images_array = [
+'amazinggrace.jpg', 
+'angel.jpg',
+'aromaticelixir.jpg',
+'beverlyhills.jpg',
+'bottledemotion.jpg',
+'cabbotine.jpg',
+'charlie.jpg',
+'cool.jpg',
+'coolwater.jpg',
+'countryapple.jpg', 
+'cukemelon.jpg',
+'curve.jpg',
+'designerimposter.jpg',
+'devotion.jpg',
+'earlyaughts.jpg',
+'electricyouth.jpg', 
+'escadatropical.jpg',
+'exclamation.jpg',
+'fetish.jpg',
+'fierce.jpg', 
+'funeral.jpg',
+'giorgio.jpg',
+'greentea.jpg',
+'halston.jpg',
+'happy.jpg',
+'jeannate.jpg',
+'knowingpleasures.jpg', 
+'lairdutemps.jpg',
+'lemale.jpg',
+'liz.jpg',
+'loulou.jpg',
+'lovesbabysoft.jpg',
+'lovespell.jpg', 
+'marykateashely.jpg',
+'parfumsjuicebar.jpg'];
+
+    
+function getRandomImage(imgAr, path) {
+    path = path || 'images/'; // default path here
+    var num = Math.floor( Math.random() * imgAr.length );
+    var img = imgAr[ num ];
+    var imgStr = '<img src="' + path + img + '" alt = "">';
+    document.write(imgStr); document.close();
+}
