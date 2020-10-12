200ad5af77b96f3f231a29aed3e4b8b735faa48f
eforaker <35076496+eforaker@users.noreply.github.com>
2020-10-12 09:03:19 -0400

    903

---
+++ Basenote Bitch/sparkle.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+
+            $("h1").sparkle({ 
+
+                "color": ["#FFF","#FFF","#FFF"] , 
+                "minSize": 2 , 
+                "maxSize": 20 ,
+                "overlap": 10 ,
+                "count": 70,
+                "speed": 0.5
+
+            });
+
+        });
---
+++ sparkleTEST/sparkle.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+
+            $("h1").sparkle({ 
+
+                "color": ["#FFF","#FFF","#FFF"] , 
+                "minSize": 2 , 
+                "maxSize": 20 ,
+                "overlap": 20 ,
+                "count": 40,
+                "speed": 0.5
+
+            });
+
+        });
