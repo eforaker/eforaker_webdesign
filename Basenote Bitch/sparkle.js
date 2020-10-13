94b84822d0a110ff6543f52eb31f63b7e3e573ab
eforaker <35076496+eforaker@users.noreply.github.com>
2020-10-13 17:31:40 -0400

    531

--- Basenote Bitch/sparkle.js
+++ Basenote Bitch/sparkle.js
@@ -2,10 +2,10 @@ $(document).ready(function() {
 
             $("h1").sparkle({ 
 
-                "color": ["#FFF","#FFF","#FFF"] , 
+                "color": ["#FFF","#FFF","#f2d4ff"] , 
                 "minSize": 2 , 
                 "maxSize": 20 ,
-                "overlap": 10 ,
+                "overlap": 20 ,
                 "count": 70,
                 "speed": 0.5
 
