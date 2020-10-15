ed3d0f55b7f8457f5bf641ddb3080e4057a3dfb7
eforaker <35076496+eforaker@users.noreply.github.com>
2020-10-15 09:43:56 -0400

    943

--- passport/passport.js
+++
@@ -1,4 +0,0 @@
-$(document).ready(function() {
-    $('.images img').draggable();
-    $('#starplanet img').draggable();
-});
--- portfo19/portfo19.js
+++
@@ -1,4 +0,0 @@
-$(document).ready(function() {
-    $('.worklinks img').draggable();
-
-});
