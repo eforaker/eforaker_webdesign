c772d722f523deff69d9eb034522f9e5a9d48ecc
eforaker <35076496+eforaker@users.noreply.github.com>
2021-05-26 08:33:08 -0400

    833

---
+++ Portfolio/nhtmf.js
@@ -0,0 +1,8 @@
+function myFunction() {
+  var x = document.getElementById("sweet");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
